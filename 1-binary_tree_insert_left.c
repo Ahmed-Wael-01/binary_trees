6d1052cfbca507b4ac9de63272815cd63293fb21
Ahmed-Wael-01 <Xahmedwaheed@gmail.com>
2024-06-06 12:36:33 +0300

    fixing silly bug in insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	node->parent = parent;
 	node->n = value;
+	node->right = NULL;
 	if (parent->left == NULL)
 	{
 		parent->left = node;
